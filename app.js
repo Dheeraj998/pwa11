dad619c1e505b2c5fb2ee83cfba3c4b5f161ccea
Dheeraj998 <45422814+Dheeraj998@users.noreply.github.com>
2019-01-12 23:14:15 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import login from './pages/login.js';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={login}/>
+          
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
